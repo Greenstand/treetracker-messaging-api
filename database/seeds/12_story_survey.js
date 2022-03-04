33f67e0db5623c5e0f9adfca90d515015dc1b1d3
Zaven Arra <matthew.fullstackgis@gmail.com>
2022-03-04 14:33:27 -0800

    chore: improve survey story seed

--- database/seeds/12_story_survey.js
+++ database/seeds/12_story_survey.js
@@ -2,13 +2,15 @@ const { v4: uuid } = require('uuid');
 const authorSeed = require('./01_table_author');
 
 const recipientId = authorSeed.author_two_id;
+const recipientId2 = authorSeed.author_two_id;
+const recipientId3 = authorSeed.author_two_id;
 const authorId = authorSeed.author_one_id;
 const messageId = uuid();
 const surveyId = uuid();
 const title = 'Number of trees planted today';
 
 const seed = async function (knex) {
-  
+
   const survey = {
     id: surveyId,
     title,
@@ -23,26 +25,114 @@ const seed = async function (knex) {
   }
   await knex('survey_question').insert(surveyQuesetion1)
 
-  const content = {
-    type: 'survey',
-    author_id: authorId,
-    subject: 'Number of trees planted today',
-    survey_id: surveyId,
-    survey_response: JSON.stringify(['1']),
-    composed_at: '2022-01-22',
-  };
-  const contentId = (await knex('content')
-    .insert(content)
-    .returning('id'))[0];
+  {
+    const content = {
+      type: 'survey',
+      author_id: authorId,
+      subject: 'Number of trees planted today',
+      survey_id: surveyId,
+      composed_at: '2022-01-22',
+    };
+    const contentId = (await knex('content')
+      .insert(content)
+      .returning('id'))[0];
 
-  const message = {
-    id: messageId,
-    content_id: contentId,
-    sender_id: authorId,
-    recipient_id: recipientId,
-  };
-  await knex('message').insert(message).returning('id');
+    {
+      const message = {
+        id: messageId,
+        content_id: contentId,
+        sender_id: authorId,
+        recipient_id: recipientId,
+      };
+      await knex('message').insert(message).returning('id');
+    }
 
+    {
+      const message = {
+        id: uuid(),
+        content_id: contentId,
+        sender_id: authorId,
+        recipient_id: recipientId2,
+      };
+      await knex('message').insert(message).returning('id');
+    }
+
+    {
+      const message = {
+        id: uuid(),
+        content_id: contentId,
+        sender_id: authorId,
+        recipient_id: recipientId3,
+      };
+      await knex('message').insert(message).returning('id');
+    }
+  }
+
+  {
+    const content = {
+      type: 'survey',
+      author_id: recipientId,
+      subject: 'Number of trees planted today',
+      survey_id: surveyId,
+      survey_response: JSON.stringify(['1']),
+      composed_at: '2022-01-22',
+    };
+    const contentId = (await knex('content')
+      .insert(content)
+      .returning('id'))[0];
+
+    const message = {
+      id: uuid(),
+      content_id: contentId,
+      sender_id: authorId,
+      recipient_id: recipientId,
+    };
+    await knex('message').insert(message).returning('id');
+  }
+
+  {
+    const content = {
+      type: 'survey',
+      author_id: recipientId2,
+      subject: 'Number of trees planted today',
+      survey_id: surveyId,
+      survey_response: JSON.stringify(['10']),
+      composed_at: '2022-01-22',
+    };
+    const contentId = (await knex('content')
+      .insert(content)
+      .returning('id'))[0];
+
+    const message = {
+      id: uuid(),
+      content_id: contentId,
+      sender_id: authorId,
+      recipient_id: recipientId,
+    };
+    await knex('message').insert(message).returning('id');
+  }
+
+  {
+    const content = {
+      type: 'survey',
+      author_id: recipientId3,
+      subject: 'Number of trees planted today',
+      survey_id: surveyId,
+      survey_response: JSON.stringify(['1000']),
+      composed_at: '2022-01-22',
+    };
+    const contentId = (await knex('content')
+      .insert(content)
+      .returning('id'))[0];
+
+    const message = {
+      id: uuid(),
+      content_id: contentId,
+      sender_id: authorId,
+      recipient_id: recipientId,
+    };
+    await knex('message').insert(message).returning('id');
+  }
 }
 
 module.exports = {
